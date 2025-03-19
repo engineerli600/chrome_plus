18644a1cbf380b9eee86fa72fdf53acfc0a60dd7
ZhiHua Li <engineerli600@outlook.com>
2025-03-19 12:58:29 +0800

    Update tabbookmark.h

--- src/tabbookmark.h
+++ src/tabbookmark.h
@@ -213,8 +213,7 @@ int HandleLeftClick(WPARAM wParam, PMOUSEHOOKSTRUCT pmouse) {
   bool keep_tab = IsNeedKeep(top_container_view);  // 检查是否需要保留标签页
 
   if (is_on_one_tab && is_on_close_button && keep_tab) {
-    ExecuteCommand(IDC_NEW_TAB, hwnd);
-    ExecuteCommand(IDC_CLOSE_TAB, hwnd);    
+    SendKey(VK_MBUTTON); 
     return 1;  // 返回 1，表示处理了事件且不关闭标签页
   }
 

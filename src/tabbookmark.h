7aab4bbf69f6b3063c223b7974825a1007ea3e14
engineer600 <engineer600@outlook.com>
2025-04-12 18:37:28 +0800

    update

--- src/tabbookmark.h
+++ src/tabbookmark.h
@@ -290,7 +290,8 @@ int HandleRightClickOnNewTabButton(WPARAM wParam, PMOUSEHOOKSTRUCT pmouse) {
       Sleep(100); // 等待100ms
       
       // 模拟鼠标释放
-      mouse_event(MOUSEEVENTF_LEFTUP, original_pt.x, original_pt.y, 0, 0);
+      //mouse_event(MOUSEEVENTF_LEFTUP, original_pt.x, original_pt.y, 0, 0);
+      SendKey(VK_LBUTTON);
     }).detach();
 
 

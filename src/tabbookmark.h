38954e48fa14ea3880b9661e0942226630946218
engineer600 <engineer600@outlook.com>
2025-04-15 22:20:07 +0800

    update

--- src/tabbookmark.h
+++ src/tabbookmark.h
@@ -22,7 +22,7 @@
 #define IDC_COPY_URL 34060
 
 #define IDC_RECENT_TABS_MENU 40239
-
+#define IDC_SHOW_HISTORY_CLUSTERS_SIDE_PANEL 40025
 
 
 HHOOK mouse_hook = nullptr;
@@ -319,7 +319,7 @@ int HandleRightClickButton(WPARAM wParam, PMOUSEHOOKSTRUCT pmouse) {
     SendKey(VK_MBUTTON);
     return 1;
   } else if (is_on_bookmark_history || !IsPressed(VK_SHIFT) ) {
-    ExecuteCommand(IDC_RECENT_TABS_MENU, hwnd);
+    ExecuteCommand(IDC_SHOW_HISTORY_CLUSTERS_SIDE_PANEL, hwnd);
     //SendKey(VK_MBUTTON);
     return 1;
   }

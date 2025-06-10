59dd0116ee72ac9623b3b41bd15b663cd172e1eb
engineer600 <engineer600@outlook.com>
2025-06-10 17:09:34 +0800

    update

--- src/tabbookmark.h
+++ src/tabbookmark.h
@@ -339,9 +339,11 @@ int HandleRightClickButton(WPARAM wParam, PMOUSEHOOKSTRUCT pmouse) {
   // 判断是否点击在 新建标签 按钮上
   if (is_on_new_tab_button) {
     // 配合 粘贴并搜索 扩展
-    //SendKey(VK_CONTROL, VK_SHIFT, 'V');
-    SendKey(VK_MBUTTON);
-    RestoreFocus(pt, 50, 0);
+    SendKey(VK_CONTROL, VK_SHIFT, 'V');
+    //SendKey(VK_MBUTTON);
+    
+    // 向右移动50px后左键点击（默认）
+    RestoreFocus(pt, 50, 0, LBUTTON);
     return 1;
     // 判断是否点击在 搜索标签页 按钮上
   } else if (is_on_search_tab_button) {

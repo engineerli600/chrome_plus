ab8e93a720c5edf066517de6f4f2b5bc7c9f24dc
engineer600 <engineer600@outlook.com>
2026-01-12 17:16:55 +0800

    update

--- src/tabbookmark.h
+++ src/tabbookmark.h
@@ -373,8 +373,7 @@ int HandleRightClickButton(WPARAM wParam, PMOUSEHOOKSTRUCT pmouse) {
       SendKey(VK_CONTROL, 'V');
       //Sleep(50);
       SendKey(VK_RETURN);
-      Sleep(50);
-      RestoreFocus(pt, 50, 0, LBUTTON);
+      RestoreFocus(pt, 0, 50, LBUTTON);
     }
 
     // 配合 粘贴并搜索 扩展
